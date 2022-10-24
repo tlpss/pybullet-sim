fc2f71faf563ade48b1bc866e7eebcf53d6ec1a6
tlips <thomas.lips@ugent.be>
2022-10-24 09:53:08 +0200

    dump current envs

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="pybullet-sim",
+    version="0.0.1",
+    author="Thomas Lips",
+    author_email="thomas.lips@ugent.be",
+    description="robot manipulation simulation environments in pybullet",
+    packages=["pybullet_sim"],
+    install_requires=[
+        # requirements.txt used as it is not possible -e install?
+        #  and ur_ikfast requires this.
+        "ur_ikfast",
+    ]
+)
